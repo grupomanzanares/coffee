85c8164e100d79d2618f68bab05086fa4538341c
ingsandravillamizar <104092220+ingsandravillamizar@users.noreply.github.com>
2024-09-26 07:46:12 -0500

    Inicio app recoleccion cafe en fincas

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,30 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.coffee.starter',
   appName: 'coffee',
-  webDir: 'www'
-};
+  webDir: 'www',
+  plugins: {
+    CapacitorSQLite: {
+      iosDatabaseLocation: 'Library/CapacitorDatabase',
+      iosIsEncryption: true,
+      iosKeychainPrefix: 'angular-sqlite-app-starter',
+      iosBiometric: {
+        biometricAuth: false,
+        biometricTitle : "Biometric login for capacitor sqlite"
+      },
+      androidIsEncryption: true,
+      androidBiometric: {
+        biometricAuth : false,
+        biometricTitle : "Biometric login for capacitor sqlite",
+        biometricSubTitle : "Log in using your biometric"
+      },
+      electronIsEncryption: true,
+      electronWindowsLocation: "C:\\ProgramData\\CapacitorDatabases",
+      electronMacLocation: "/Volumes/Development_Lacie/Development/Databases",
+      electronLinuxLocation: "Databases"
+    }
+  }
+}
 
 export default config;
